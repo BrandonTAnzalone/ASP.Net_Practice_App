868191084fa948e97022dbff07e156822aa4ba83
Brandon Anzalone <brandon.anzalone@outlook.com>
2022-05-20 10:16:00 -0500

    First Updates 5/20/22

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+
+    console.log("Hello World");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        this.log("You clicked on " + $(this).innerText());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
+
+
+
+
+});
